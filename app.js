c4f3b3763686fa647b42ddad45d1a218db10def3
Vinícius Hoffmann Vieira <vinicius-avaiano@hotmail.com>
2023-08-15 21:05:05 -0300

    testando

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("O sistema esta prestes a iniciar através do banco de dados");
 prompt('Escreva seu nome');
+console.log("Testando o retorno");
